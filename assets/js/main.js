2d6440afd7959536fda262dd6970c528cf265e2b
Finzaiko <finzaiko@gmail.com>
2018-07-14 14:34:48 +0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var size = {
+    width: window.innerWidth || document.body.clientWidth,
+    height: window.innerHeight || document.body.clientHeight
+}
+
+
+$(function() {
+    // var nHeight = size.height - 462 - 100;
+    // $(".main-container").css('min-height', nHeight+'px');
+});
